c675e4dee5b45647198e4dc8b82ccf104d739862
sgp0wer <grisha-savellev@mail.ru>
2025-07-24 14:33:18 +0500

    Updated Game Systems
    Demo finished

---
+++ BeyondOfHonor_CompetitiveFPS/Assets/Scripts/Network/NetworkController.cs
@@ -0,0 +1,25 @@
+﻿using FishNet.Object;
+using UnityEngine;
+using FishNet.Connection;
+
+public class NetworkController : NetworkBehaviour
+{
+    [SerializeField] private GameObject cameraHolder;
+    [SerializeField] private MonoBehaviour[] localOnlyScripts;
+
+    public override void OnOwnershipClient(NetworkConnection prevOwner)
+    {
+        base.OnOwnershipClient(prevOwner);
+
+        if (!IsOwner) return;
+
+        if (cameraHolder != null)
+            cameraHolder.SetActive(true);
+
+        foreach (var s in localOnlyScripts)
+            s.enabled = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
